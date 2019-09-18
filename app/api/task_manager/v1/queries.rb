2929feb6a027749f831b9d8d465e7609bc78ffef
superpuper777 <superfevrik@gmail.com>
2019-09-18 20:24:11 +0300

    Fixing some queries

--- app/api/task_manager/v1/queries.rb
+++ app/api/task_manager/v1/queries.rb
@@ -8,20 +8,18 @@ module TaskManager
       resource :queries do
         desc 'Return all statuses, not repeating, alphabetically ordered. '
         get '/statuses' do
-          statuses = Task.select(:status).distinct #.order('status ASC')
+          Task.select(:status).distinct #.order('status ASC')
         end
 
         desc 'Return the count of all tasks in each project, order by tasks count descending. '
         get '/counts' do
-          #put project tasks count
-          # count = Task.where("project_id = '39'").select(:id).count
-          # Task.select(:id, :project_id).order(project_id: :desc)
-          counts = Task.group(:project_id).count
+          Task.group(:project_id).count
         end
 
         desc 'Return the count of all tasks in each project, order by projects names. '
         get '/names' do
-          Task.joins(:project).select(:id , :name).order(name: :asc)
+          # Task.joins(:project).select(:id , :name).order(name: :asc)
+          Project.joins(:tasks).select(:id, :name).order(name: :asc)
         end
 
         desc 'Return the tasks for all projects having the name beginning with “N” letter. '
@@ -36,7 +34,7 @@ module TaskManager
 
         desc 'Return the list of project names having more than 10 tasks in status ‘completed’. Order by project_id. '
         get '/completed_status' do
-          Task.joins(:project).where("status = 1").select(:id).count
+          Project.joins(:tasks).where("status = 1").select(:name).order(project_id: :asc)
         end
 
       end
